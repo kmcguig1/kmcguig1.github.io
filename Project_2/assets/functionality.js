4b5e7797b07abff9726bdd15c34d027335e1ecea
Kayleigh McGuigan <kmcguig1@nd.edu>
2018-10-08 17:11:46 -0400

    Project 2 partial coded wireframe
    Coded wireframe, almost full functionality. Only parts not enitrely functional are the popups, both on the home page and the game.

---
+++ Project_2/assets/functionality.js
@@ -0,0 +1,138 @@
+//Suitcase
+
+$(document).ready(function () {
+    $('.suitcase_button').on('click', function () {
+        $('.suitcase').animate({
+            opacity: '0',
+        })
+    });
+});
+
+$(document).ready(function () {
+    $('.suitcase_button').on('click', function () {
+        $('.Check1').animate({
+            opacity: '1',
+        })
+    });
+});
+
+//$(document).ready(function () {
+//    $('.suitcase_button').on('click', function () {
+//        $('.person').animate({
+//            opacity: '0',
+//        })
+//    });
+//});
+
+$(document).ready(function () {
+    $('.suitcase_button').on('click', function () {
+        $('.person2').animate({
+            opacity: '1',
+        })
+    });
+});
+
+//Passport
+
+$(document).ready(function () {
+    $('.passport_button').on('click', function () {
+        $('.passport').animate({
+            opacity: '0',
+        })
+    });
+});
+
+$(document).ready(function () {
+    $('.passport_button').on('click', function () {
+        $('.Check3').animate({
+            opacity: '1',
+        })
+    });
+});
+
+$(document).ready(function () {
+    $('.passport_button').on('click', function () {
+        $('.person3').animate({
+            opacity: '1',
+        })
+    });
+});
+
+//Car
+
+$(document).ready(function () {
+    $('.car_button').on('click', function () {
+        $('.car').animate({
+            left: '750px',
+        })
+    });
+});
+
+$(document).ready(function () {
+    $('.car_button').on('click', function () {
+        $('.Check5').animate({
+            opacity: '1',
+        })
+    });
+});
+
+$(document).ready(function () {
+    $('.car_button').on('click', function scrollWin() {
+        window.scrollTo(2300,0);({
+        })
+    });
+});
+
+//Boarding Pass
+
+$(document).ready(function () {
+    $('.bp_button').on('click', function () {
+        $('.boarding_pass').animate({
+            opacity: '0',
+        })
+    });
+});
+
+$(document).ready(function () {
+    $('.bp_button').on('click', function () {
+        $('.Check2').animate({
+            opacity: '1',
+        })
+    });
+});
+
+$(document).ready(function () {
+    $('.bp_button').on('click', function () {
+        $('.person4').animate({
+            opacity: '1',
+        })
+    });
+});
+
+//Security
+
+$(document).ready(function () {
+    $('.security_button').on('click', function () {
+        $('.person').animate({
+            left: '1000px',
+        })
+    });
+});
+
+$(document).ready(function () {
+    $('.security_button').on('click', function () {
+        $('.Check4').animate({
+            opacity: '1',
+        })
+    });
+});
+
+//Boarding
+
+$(document).ready(function () {
+    $('.board_button').on('click', function () {
+        $('.Check6').animate({
+            opacity: '1',
+        })
+    });
+});
